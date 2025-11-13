25b9386b81857bff3b80ec4ce44fe3e06fce0b55
illan seghir <illan.seghir@enseirb-matmeca.fr>
2025-11-13 11:30:15 +0100

    ajout fonction pour calculer la platitude spectrale selon la méthode + main de la dernière partie du preli 1)

---
+++ PRELI_1/platitude_spectrale.m
@@ -0,0 +1,35 @@
+function [moy_SF, ecarttype_SF] = platitude_spectrale(N, sigma, K, methode, window_1, recouvrement_1, Nfft, fs)
+
+    Plat_spect = zeros(1, K);
+
+    for k = 1:K
+
+        % 1) bruit
+        x = sigma * randn(1, N);
+
+        % 2) spectre estimé selon la méthode
+        switch methode
+
+            case 'autocorr'
+                Rx = xcorr(x, 'biased');
+                P = (abs(fftshift(fft(Rx))).^2) / N;
+
+            case 'welch'
+                P = pwelch(x, window_1, recouvrement_1, Nfft, fs);
+
+            otherwise
+                error('Méthode inconnue : utilisez ''autocorr'' ou ''welch''');
+        end
+
+        % platitude spectrale
+        P_moy_arith = mean(P);
+        P_moy_geo   = exp(mean(log(P + eps)));
+        Plat_spect(k) = P_moy_geo / P_moy_arith;
+
+    end
+
+    % stats sur les K réalisations
+    moy_SF       = mean(Plat_spect);
+    ecarttype_SF = std(Plat_spect);
+
+end
